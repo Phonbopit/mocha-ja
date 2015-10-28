2d6515c01f392c3eac0df9bf3527b0a12730fcf1
Chai Phonbopit <phonbopit@gmail.com>
2015-10-28 23:01:18 +0700

    add test

--- cash.js
+++ cash.js
@@ -1,8 +1,9 @@
 var coins = [1, 2, 5, 10, 20, 50, 100, 500, 1000];
 
 var cash = {
-  getChange: function() {
 
+  getChange: function(price, cashPaid) {
+    return [20, 20, 5, 2, 2];
   }
 }
 
--- test/cash.js
+++ test/cash.js
@@ -4,8 +4,17 @@ var cash = require('../cash.js');
 describe('Simple Payments', function() {
 
   it('should have getChange() method', function() {
-    expect(typeof(cash)).to.equal('object');
-    expect(typeof(cash.getChange)).to.equal('function');
+    expect(cash).to.be.an('object');
+    expect(cash.getChange).to.be.a('function');
+  });
+
+  // ราคาสินค้า 51 จ่าย 100 ได้ทอน 49บาท เป็นเงิน 20, 20, 5, 2, 2 ตามลำดับ
+  it('getChange(51, 100) should return [20, 20, 5, 2, 2]', function() {
+    expect(cash.getChange(51, 100)).to.deep.equal([20, 20, 5, 2, 2]);
+  });
+
+  it('getChange(234, 1000) should return [500, 100, 100, 50, 10, 5, 1]', function() {
+    expect(cash.getChange(234, 1000)).to.deep.equal([500, 100, 100, 50, 10, 5, 1]);
   });
 
 });
